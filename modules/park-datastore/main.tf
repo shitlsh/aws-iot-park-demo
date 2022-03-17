89fd9218bbdc1f266d7651577429bce110e8c29e
Tailong <shitlsh@126.com>
2022-03-17 16:36:20 +0800

    fix rule error can not get client id for pk

--- modules/park-datastore/main.tf
+++ modules/park-datastore/main.tf
@@ -24,7 +24,7 @@ resource "aws_iot_topic_rule" "park-datastore-rule" {
   dynamodb {
     role_arn        = aws_iam_role.iam_role_for_datastore.arn
     hash_key_field  = aws_dynamodb_table.park_db.hash_key
-    hash_key_value  = "$${clientId}"
+    hash_key_value  = "$${topic()}"
     range_key_field = aws_dynamodb_table.park_db.range_key
     range_key_value = "$${timestamp()}"
     table_name      = aws_dynamodb_table.park_db.name
